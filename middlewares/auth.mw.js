2213ee1b3625643d3a823352233701ffb40f1485
Vishwa <kankvish@gmail.com>
2024-02-27 20:27:21 +0530

    login with m/w

--- middlewares/auth.mw.js
+++ middlewares/auth.mw.js
@@ -47,6 +47,22 @@ const verifySignUpBody = async (req, res, next)=>{
     }
 }
 
+const verifySignInBody = async (req, res, next)=>{
+
+    if(!req.body.userId){
+        return res.status(400).send({
+            message : "userId is not provided"
+        })
+    }
+    if(!req.body.password){
+        return res.status(400).send({
+            message : "password is not provided"
+        })
+    }
+    next()
+}
+
 module.exports = {
-    verifySignUpBody : verifySignUpBody
+    verifySignUpBody : verifySignUpBody,
+    verifySignInBody : verifySignInBody
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,5 +14,5 @@ module.exports = (app)=>{
      * route for
      * POST localhost:8888/ecomm/api/v1/auth/signin
      */
-    app.post("/ecomm/api/v1/auth/signin",authController.signin )
+    app.post("/ecomm/api/v1/auth/signin",[authMW.verifySignInBody],authController.signin )
 }
